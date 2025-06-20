fdaef9c5b6101796356e7f13a116e0f5568711b7
LEO CHOU <leotimjack@gmail.com>
2025-06-20 21:45:05 +0800

    feat: add getOrderReview in user.js

--- routes/users.js
+++ routes/users.js
@@ -24,9 +24,9 @@ router.get('/orderReview', auth, handleErrorAsync(usersController.getOrderReview
 router.post('/membership/order', auth, handleErrorAsync(usersController.postCreateOrder));
 router.get('/membership/orders', auth, handleErrorAsync(usersController.getUserOrders));
 router.put('/checkout', auth, handleErrorAsync(usersController.putCheckout));
-router.get('/membership/:order_id', auth, handleErrorAsync(usersController.getUserOrderDetail));
 router.get('/discount', auth, handleErrorAsync(usersController.getDiscount));
 router.get('/orderReview', auth, handleErrorAsync(usersController.getOrderReview));
-router.post('/membership/order', auth, handleErrorAsync(usersController.postCreateOrder));
+router.put('/membership/order', auth, handleErrorAsync(usersController.postCreateOrder));
+router.get('/membership/:order_id', auth, handleErrorAsync(usersController.getUserOrderDetail));
 
 module.exports = router;
